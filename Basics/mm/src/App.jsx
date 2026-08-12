d3c05ac489cb2ce8553a0c6a23365d80d478e3f3
Allan <altonallan47@gmail.com>
2026-08-12 13:43:35 +0300

    chore: Depencencies

--- Basics/mm/src/App.jsx
+++ Basics/mm/src/App.jsx
@@ -1,9 +1,22 @@
 import * as THREE from "three";
 
+const canvas = document.createElement("canvas");
+
 const scene = new THREE.Scene();
 
-const App = () => {
-  return <div></div>;
-};
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-export default App;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
