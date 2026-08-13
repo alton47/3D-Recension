1e3bcc96e2726e0c22c308b2a145e7e538b796ff
Allan <altonallan47@gmail.com>
2026-08-13 13:24:41 +0300

    chore: Dance around with Orthographic camera

--- Basics/Cameras/src/script.js
+++ Basics/Cameras/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 //Cursor
 
@@ -9,7 +10,7 @@ const cursor = {
 
 window.addEventListener("mousemove", (event) => {
   cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height) - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
 });
 
 // Canvas
@@ -19,8 +20,11 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffdd00 });
+const geometry = new THREE.RingGeometry(1, 5, 32);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xffff00,
+  side: THREE.DoubleSide,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -32,21 +36,13 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(55, sizes.width / sizes.height);
-// const aspectRatio = sizes.width / sizes.height;
-// const camera = new THREE.OrthographicCamera(
-//   -1.333 * aspectRatio,
-//   1.333 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   100,
-// );
-
-// camera.position.x = 2;
-// camera.position.z = 0.01;
 camera.position.z = 2;
-camera.lookAt(mesh.position);
 scene.add(camera);
+
+//Orbit control
+const controls = new OrbitControls(camera, canvas);
+controls.enablePan = true;
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -61,11 +57,14 @@ const tick = () => {
   //mesh.rotation.z = Math.sin(time) * 2;
 
   //Update the camera
-  camera.position.x = cursor.x * 3;
-  //camera.position.y = cursor.y;
-  camera.lookAt(mesh.position);
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  //   camera.position.y = cursor.y * 5;
+  //   camera.lookAt(mesh.position);
 
-  renderer.render(scene, camera);
+  controls.update();
+
+  renderer.render(scene, camera); /*  */
   window.requestAnimationFrame(tick);
 };
 
