1862d51455d98ab155faffa5ae99c087e0097811
Allan <altonallan47@gmail.com>
2026-08-13 15:05:57 +0300

    chore: Fix the Pixel ratio

--- Basics/Cameras/src/script.js
+++ Basics/Cameras/src/script.js
@@ -58,7 +58,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Animation
 let time = 0;
