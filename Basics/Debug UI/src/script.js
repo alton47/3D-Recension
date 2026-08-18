9fca7b94f030ae9b50baa98c7311cee004a1d47f
Allan <altonallan47@gmail.com>
2026-08-18 15:53:36 +0300

    chore: Install lil-gui

--- Basics/Debug UI/src/script.js
+++ Basics/Debug UI/src/script.js
@@ -1,5 +1,9 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
+
+// Debug
+const gui = new GUI();
 
 /**
  * Base
