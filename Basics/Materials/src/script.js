7301f7a0b78daf102882549f742119af3df7a9e0
Allan <altonallan47@gmail.com>
2026-08-24 17:42:04 +0300

    Add an Envrt map

--- Basics/Materials/src/script.js
+++ Basics/Materials/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
 /**
  * Base
@@ -10,23 +11,73 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Texture Loader
+const textureLoader = new THREE.TextureLoader();
+
+// With loadingManager
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onStart = () => {
+  console.log("Loading started");
+};
+
+loadingManager.onLoad = () => {
+  console.log("Loading complete");
+};
+
+loadingManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+  console.log(
+    `Loading file: ${url}. Loaded ${itemsLoaded} of ${itemsTotal} files.`,
+  );
+};
+
+loadingManager.onError = (url) => {
+  console.error(`There was an error loading ${url}`);
+};
+
+const metal = textureLoader.load("/textures/matcaps/3.png");
+const cartoon = textureLoader.load("/textures/matcaps/6.png");
+const saturn = textureLoader.load("/textures/matcaps/5.png");
+const green = textureLoader.load("/textures/matcaps/7.png");
+const door = textureLoader.load("/textures/door/color.jpg");
+
+// To get real sRGB
+door.colorSpace = THREE.SRGBColorSpace;
+green.colorSpace = THREE.SRGBColorSpace;
+saturn.colorSpace = THREE.SRGBColorSpace;
+door.encoding = THREE.sRGBEncoding;
+
 // Objects
+
+const material = new THREE.MeshMatcapMaterial();
+material.matcap = saturn;
+
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+//const sphereMaterial = new THREE.MeshBasicMaterial({ map: door });
+const sphereMesh = new THREE.Mesh(sphereGeometry, material);
 scene.add(sphereMesh);
 sphereMesh.position.x = -2;
 
-const planeGeometry = new THREE.PlaneGeometry(1, 1);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-scene.add(planeMesh);
+// const planeGeometry = new THREE.PlaneGeometry(1, 1);
+// //const planeMaterial = new THREE.MeshBasicMaterial({ map: green });
+// const planeMesh = new THREE.Mesh(planeGeometry, material);
+// scene.add(planeMesh);
+
+const sphere = new THREE.SphereGeometry(1, 32, 32);
+//const sphereMaterial = new THREE.MeshBasicMaterial({ map: door });
+const sphereMe = new THREE.Mesh(sphere, material);
+scene.add(sphereMe);
+sphereMe.position.x = 0.2;
 
 const torusGeometry = new THREE.TorusGeometry(0.5, 0.2, 32, 64);
-const torusMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+//const torusMaterial = new THREE.MeshBasicMaterial({ map: saturn });
+const torusMesh = new THREE.Mesh(torusGeometry, material);
 scene.add(torusMesh);
 torusMesh.position.x = 2;
+
+const colorTexture = textureLoader.load("/textures/leza.png");
+colorTexture.magFilter = THREE.NearestFilter;
+colorTexture.generateMipmaps = false;
 
 /**
  * Sizes
@@ -65,6 +116,14 @@ camera.position.y = 1;
 camera.position.z = 2;
 scene.add(camera);
 
+// Enviroment map
+const rgbeLoader = new RGBELoader();
+rgbeLoader.load("/textures/environmentMap/2k.hdr", (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.background = texture;
+  scene.environment = texture;
+});
+
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
@@ -87,7 +146,7 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   sphereMesh.rotation.y = elapsedTime * 1;
-  planeMesh.rotation.y = elapsedTime * 1;
+  sphereMe.rotation.y = elapsedTime * 1;
   torusMesh.rotation.y = elapsedTime * 1;
 
   // Update controls
